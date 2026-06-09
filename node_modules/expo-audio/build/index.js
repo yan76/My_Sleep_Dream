ae9b15a9737d3899d55cde9b1da5b2a911edd268
Y <13822137323@163.com>
2026-06-09 17:32:50 +0800

    chore: stop tracking generated files

--- node_modules/expo-audio/build/index.js
+++
@@ -1,6 +0,0 @@
-export { PermissionStatus } from 'expo-modules-core';
-export * from './ExpoAudio';
-export * from './Audio.types';
-export * from './AudioModule.types';
-export * from './RecordingConstants';
-//# sourceMappingURL=index.js.map
--- node_modules/expo-audio/build/utils/resolveSource.d.ts
+++
@@ -1,3 +0,0 @@
-import { AudioSource } from '../Audio.types';
-export declare function resolveSource(source?: AudioSource | string | number | null): AudioSource | null;
-//# sourceMappingURL=resolveSource.d.ts.map
